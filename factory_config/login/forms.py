f28480a982aa14f865262920559b036106c218bf
AminPourvadi <github.amin.pourvadi@gmail.com>
2025-01-24 21:53:53 +0330

    Delete factory_config directory

--- factory_config/login/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    email = forms.EmailField(label="ایمیل", max_length=255)
-    password = forms.CharField(label="رمز عبور", widget=forms.PasswordInput)
--- factory_config/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import user_login , api_login
-
-urlpatterns = [
-    path('form/', user_login, name='login'),
-    path('api/', api_login, name='api_login'),
-]
